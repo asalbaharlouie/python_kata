cb4fa4387ac4a25ef741178ad5ee236121603a7d
asalbaharlouie <asalbaharlouie@gmail.com>
2026-09-06 09:36:10 +0200

    remove duplicates

---
+++ tests/test_remove_duplicates.py
@@ -0,0 +1,16 @@
+def remove_duplicates(items):
+    seen_list = []
+    seen_set = set()
+    for item in items:
+        if item not in seen_set:
+            seen_list.append(item)
+            seen_set.add(item)
+
+    return seen_list 
+
+def test_remove_duplicates():
+    assert remove_duplicates([1, 2, 3, 2, 1]) == [1, 2, 3]
+
+test_remove_duplicates()
+
+print(remove_duplicates([1, 2, 4, 4, 3, 1, 2, 2]))
